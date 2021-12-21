c457f8d2e14794f6e3505d46b933c72f8e7e2dfa
Vedant <vedantatwork21@gmail.com>
2021-12-21 19:18:07 +0530

    8
    Throw Error

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 //  return alert(`${message} everyone! `);
 //}
 
-let greeting = (message) => alert(`${message} everyone!`);
+//let greeting = (message) => alert(`${message} everyone!`);
 
-greeting('Good Morning');
+//greeting('Good Morning');
+
+let createBlog = (title, body) => {
+  if (!title) {
+    throw new Error('A title required');
+  }
+
+  if (!body) {
+    throw new Error("Body can't be empty");
+  }
+  return '${title}-${body}';
+};
+
+createBlog();
