a4076c34bb50110b77e378e69c02b490ea5d4974
Vedant <vedantatwork21@gmail.com>
2021-12-21 08:31:31 +0530

    3

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ let age = prompt("Guess Vedant's age..");
 
 //old way
 
-let result = fname + ' ' + lname + ' is ' + age + ' years old';
+//let result = fname + ' ' + lname + ' is ' + age + ' years old';
+//alert(result);
+
+//using template string
+let result = ` ${fname} ${lname} is ${age} years old`;
 alert(result);
