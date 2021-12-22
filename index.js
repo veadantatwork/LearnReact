5291c6de092a68843ab383d1020c6f8ae5b2fc90
Vedant <vedantatwork21@gmail.com>
2021-12-22 08:07:51 +0530

    12
    error , where does this point

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-//Arrow function
-function sayHi() {}
+let india = {
+  //add property
+  mountains: ['Aravali', 'Western Ghats', 'Eastern Ghats'],
 
-//this refers to enclosing function^^
+  //add method
+  printWithDash: function () {
+    setTimeout(function () {
+      console.log(this.mountains.join('-'));
+    }, 3000);
+  },
+};
 
-console.log(this);
+india.printWithDash();
